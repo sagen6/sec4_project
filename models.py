a885316ed3f4d8bae07571d77ef6a322d2dfd894
sagen6 <sagensoren1432@gmail.com>
2024-05-06 22:32:11 +0530

    op

--- models.py
+++ models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-# Create your models he
 class op(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-
-class teacher(models.Model):
-  name = models.CharField(max_length=255)
-  mobile=models.IntegerField()
-  email=models.EmailField( max_length=254)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
